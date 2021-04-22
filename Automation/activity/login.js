c5253233502a7ac350108a5c89e5b9fcb3daaaf4
Sushant <sushantberiwal@gmail.com>
2021-04-22 20:48:21 +0530

    all challenges added

--- Automation/activity/login.js
+++ Automation/activity/login.js
@@ -36,6 +36,7 @@ let pw = "12345678";
     tab.waitForNavigation({ waitUntil: "networkidle2" }),
     manageChallengeLi.click(),
   ]);
+  await tab.waitForSelector(".btn.btn-green.backbone.pull-right" , {visible:true});
   let createChallengeButton = await tab.$(".btn.btn-green.backbone.pull-right");
   let createChallengeLink = await tab.evaluate(function (elem) {
     return elem.getAttribute("href");
@@ -43,17 +44,55 @@ let pw = "12345678";
   console.log(createChallengeLink);
   createChallengeLink = `https://www.hackerrank.com${createChallengeLink}`;
 
-//   for(let i=0 ; i<challenges.length; i++){
+  for(let i=0 ; i<challenges.length; i++){
       let newTab = await browser.newPage();
-      await challengeAdd(newTab , challenges[0] , createChallengeLink);
-//   }
-
+      await challengeAdd(newTab , challenges[i] , createChallengeLink);
+  }
 })();
 
 // add a single challenge
 async function challengeAdd(newTab , challenge , createChallengeLink){
+    // {
+    //     "Challenge Name": "Pep_Java_1GettingStarted_1IsPrime",
+    //     "Description": "Question 1",
+    //     "Problem Statement": "Take as input a number n. Determine whether it is prime or not. If it is prime, print 'Prime' otherwise print 'Not Prime.",
+    //     "Input Format": "Integer",
+    //     "Constraints": "n <= 10 ^ 9",
+    //     "Output Format": "String",
+    //     "Tags": "Basics",
+    //     "Testcases": [
+    //       {
+    //         "Input": "7",
+    //         "Output": "Prime"
+    //       },
+    //       {
+    //         "Input": "9",
+    //         "Output": "Not Prime"
+    //       }
+    //     ]
+    //   }
+    let challengeName =  challenge["Challenge Name"];
+    let description = challenge["Description"];
+    let problemStatement = challenge["Problem Statement"];
+    let inputFormat = challenge["Input Format"];
+    let constrainsts = challenge["Constraints"];
+    let outputFormat = challenge["Output Format"];
+    let tags = challenge["Tags"];
+
     await newTab.goto(createChallengeLink);
     // await newTab.waitForTimeout(5000);
     // enter details of challenge in ui from challenge object
+    await newTab.waitForSelector('#name' , {visible:true});
+    await newTab.type("#name" , challengeName);
+    await newTab.type('#preview' , description);
+    await newTab.waitForSelector('#problem_statement-container .CodeMirror textarea' , {visible:true});
+    await newTab.type('#problem_statement-container .CodeMirror textarea' , problemStatement);
+    await newTab.type('#input_format-container .CodeMirror textarea' , inputFormat);
+    await newTab.type('#constraints-container .CodeMirror textarea' , constrainsts);
+    await newTab.type('#output_format-container .CodeMirror textarea' , outputFormat);
+    await newTab.type('#tags_tag' , tags);
+    await newTab.keyboard.press("Enter");
+    await newTab.click('.save-challenge.btn.btn-green');
+    await newTab.waitForTimeout(5000);
     await newTab.close();
 }
