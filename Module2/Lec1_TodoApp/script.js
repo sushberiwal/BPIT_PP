49380630714ada96d365a351ea98367ec4ab2dc9
Sushant <sushantberiwal@gmail.com>
2021-04-24 16:46:42 +0530

    delete todo function added !!

--- Module2/Lec1_TodoApp/script.js
+++ Module2/Lec1_TodoApp/script.js
@@ -1,19 +1,26 @@
 let todoInput = document.querySelector(".todo-input");
 let todosList = document.querySelector(".todo-list");
 
-todoInput.addEventListener("keypress" , function(event){
-    if(event.key == "Enter"){
-       let todo = event.target.value;
-       if(todo){
-        let li = document.createElement("li");
-        // sets the html of li
-        li.innerHTML = todo;
-        todosList.append(li);
+todoInput.addEventListener("keypress" , handleKeyPress);
 
-        event.target.value="";
-       }
-       else{
-           alert("Enter a todo !!");
-       }
-    }
-})
+function handleKeyPress(event){
+    if(event.key == "Enter"){
+        let todo = event.target.value;
+        if(todo){
+         let li = document.createElement("li");
+         li.classList.add("todo");
+         // sets the html of li
+         li.innerHTML = todo;
+         li.addEventListener("dblclick" , handleDeleteTodo);
+         todosList.append(li);
+         event.target.value="";
+        }
+        else{
+            alert("Enter a todo !!");
+        }
+     }
+}
+
+function handleDeleteTodo(e){
+    e.target.remove();
+}
